6adfe7488bc15b6a5d92d1bb0faa973c17144fa1
Sebastian Hönig <hoenigsebastian@gmail.com>
2022-01-14 00:44:21 +0100

    changed style.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createMenu } from "./modules/menu";
 import { createContact } from "./modules/contact";
 import { createFooter, createNavBar } from "./modules/standard";
 
+// TODO: Do it with event deleagtion & .target.id case-switch
+
 let content = document.getElementById("content");
 const navbar = createNavBar();
 const footer = createFooter();
@@ -26,14 +28,6 @@ menuIcon.addEventListener("click", () => {
 const contactIcon = document.getElementById("navbar-contact");
 contactIcon.addEventListener("click", () => {
   let container = createContact();
-  content.textContent = "";
-  content.append(navbar, container, footer);
-});
-
-const menuButton = document.getElementById("menu-button");
-menuButton.addEventListener("click", () => {
-  console.log("HEY");
-  let container = createMenu();
   content.textContent = "";
   content.append(navbar, container, footer);
 });
